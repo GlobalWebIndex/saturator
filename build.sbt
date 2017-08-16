a1b2d37140d617bfba6efb6bb47a70297699c516
l15k4 <liska.jakub@gmail.com>
2017-08-16 13:19:30 +0200

    exposing API + version bump

--- build.sbt
+++ build.sbt
@@ -1,17 +1,23 @@
 
-version in ThisBuild := "0.0.6"
+version in ThisBuild := "0.0.7"
 crossScalaVersions in ThisBuild := Seq("2.12.3", "2.11.8")
 organization in ThisBuild := "net.globalwebindex"
 fork in Test in ThisBuild := true
-libraryDependencies in ThisBuild ++= loggingApi ++ Seq(akkaActor, akkaTestkit, akkaPersistence, akkaPersistenceRedis, akkaKryoSerialization, scalatest)
 
 lazy val saturator = (project in file("."))
-  .aggregate(`saturator-core`, `saturator-example`)
+  .aggregate(`saturator-api`, `saturator-core`, `saturator-example`)
+
+lazy val `saturator-api` = (project in file("api"))
+  .enablePlugins(CommonPlugin)
+  .settings(name := "saturator-api")
+  .settings(publishSettings("GlobalWebIndex", "saturator-api", s3Resolver))
 
 lazy val `saturator-core` = (project in file("core"))
   .enablePlugins(CommonPlugin)
   .settings(name := "saturator-core")
-  .settings(publishSettings("GlobalWebIndex", "saturator", s3Resolver))
+  .settings(libraryDependencies ++= loggingApi ++ Seq(akkaActor, akkaPersistence, akkaPersistenceRedis, akkaKryoSerialization, akkaTestkit, scalatest))
+  .settings(publishSettings("GlobalWebIndex", "saturator-core", s3Resolver))
+  .dependsOn(`saturator-api` % "compile->compile;test->test")
 
 lazy val `saturator-example` = (project in file("example"))
   .enablePlugins(CommonPlugin, DockerPlugin)
--- core/src/main/scala/gwi/saturator/DagMock.scala
+++ core/src/main/scala/gwi/saturator/DagMock.scala
@@ -1,44 +1,8 @@
 package gwi.saturator
 
-import scala.math.Ordering
-
-trait DagPartition {
-  def pid: String
-}
-
-trait DagVertex {
-  def vid: String
-}
-
-object DagVertex {
-  protected[saturator] object State {
-    val Pending = "Pending"
-    val Complete = "Complete"
-    val InProgress = "InProgress"
-    val Failed = "Failed"
-  }
-}
-case class Dependency(p: DagPartition, sourceVertices: Set[DagVertex], targetVertex: DagVertex)
-
-object Dependency {
-  implicit def dependencyOrdering(implicit po: Ordering[DagPartition], vo: Ordering[DagVertex]) = new Ordering[Dependency] {
-    override def compare(x: Dependency, y: Dependency): Int = {
-      val partitionOrd = po.compare(x.p,y.p)
-      lazy val sourceVertexOrd = vo.compare(x.sourceVertices.head, y.sourceVertices.head)
-      lazy val targetVertexOrd = vo.compare(x.targetVertex, y.targetVertex)
-      if (partitionOrd != 0)
-        partitionOrd
-      else if (sourceVertexOrd != 0)
-        sourceVertexOrd
-      else
-        targetVertexOrd
-    }
-  }
-
-}
-
 /**
-  * !!! Test environment !!! To build Dag using just Ints instead of Vertices and Longs instead of Partitions
+  * !!! Test environment !!! it is needed in core tests and example projects to, "test->compile" dependency is awful
+  * To build Dag using just Ints instead of Vertices and Longs instead of Partitions
   */
 protected[saturator] object DagMock {
   import scala.language.implicitConversions
