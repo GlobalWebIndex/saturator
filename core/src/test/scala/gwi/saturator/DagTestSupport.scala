183cdea643f91b17f430fc4fdc8226d98259cec0
l15k4 <liska.jakub@gmail.com>
2017-11-18 14:47:38 +0100

    remove legacy docker support and move Dag Test support to better place

--- core/src/test/scala/gwi/saturator/DagTestSupport.scala
+++ core/src/test/scala/gwi/saturator/DagTestSupport.scala
@@ -1,29 +1,23 @@
 package gwi.saturator
 
-/**
-  * !!! Test environment !!! it is needed in core tests and example projects to, "test->compile" dependency is awful
-  * To build Dag using just Ints instead of Vertices and Longs instead of Partitions
-  */
-protected[saturator] object DagMock {
+trait DagTestSupport {
   import scala.language.implicitConversions
-  case class PartitionMock(pid: String) extends DagPartition
-  case class VertexMock(vid: String) extends DagVertex
 
-  implicit object VertexMockOrdering extends Ordering[DagVertex] {
+  implicit object intVertexOrdering extends Ordering[DagVertex] {
     override def compare(x: DagVertex, y: DagVertex): Int =
-      x.vid.compareTo(y.vid)
+      x.vid.toInt.compareTo(y.vid.toInt)
   }
 
-  implicit object PartitionMockOrdering extends Ordering[DagPartition] {
+  implicit object intPartitionOrdering extends Ordering[DagPartition] {
     override def compare(x: DagPartition, y: DagPartition): Int =
-      x.pid.compareTo(y.pid)
+      x.pid.toInt.compareTo(y.pid.toInt)
   }
 
-  implicit def longToPartition(i: Long): DagPartition = PartitionMock(i.toString)
-  implicit def intToPartition(i: Int): DagPartition = PartitionMock(i.toString)
+  implicit def longToPartition(i: Long): DagPartition = DagPartition(i.toString)
+  implicit def intToPartition(i: Int): DagPartition = DagPartition(i.toString)
   implicit def longsToPartitions(is: Set[Long]): Set[DagPartition] = is.map(implicitly[DagPartition](_))
   implicit def longsToPartitions(is: List[Long]): List[DagPartition] = is.map(implicitly[DagPartition](_))
-  implicit def intToVertex(i: Int): DagVertex = VertexMock(i.toString)
+  implicit def intToVertex(i: Int): DagVertex = DagVertex(i.toString)
   implicit def vertexEdgeFromInts(i: (Int,Int)): (DagVertex, DagVertex) = (i._1, i._2)
   implicit def longsToPartitionsByVertex(is: Map[Int, Set[Long]]): Map[DagVertex, Set[DagPartition]] = is.map { case (v,ps) => implicitly[DagVertex](v) -> implicitly[Set[DagPartition]](ps) }
   implicit def longsToPartitionsByVertex(is: List[(Int, List[Long])]): List[(DagVertex, List[DagPartition])] = is.map { case (v,ps) => implicitly[DagVertex](v) -> implicitly[List[DagPartition]](ps) }
