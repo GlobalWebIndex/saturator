542345a5bd74184d3ee4dd055281d28ebd77081d
l15k4 <liska.jakub@gmail.com>
2017-11-18 13:49:44 +0100

    dag should be an instance to prevent implicits overflow

--- core/src/main/scala/gwi/saturator/Dag.scala
+++ core/src/main/scala/gwi/saturator/Dag.scala
@@ -1,40 +1,40 @@
 package gwi.saturator
 
-object Dag {
+protected[saturator] case class Dag[V](edges: Set[(V,V)]) {
 
-  def root[V](edges: Set[(V,V)]): V = {
+  def root: V = {
     val ancestorLessVertices = edges.flatMap(v => Set(v._1, v._2)) -- edges.map(_._2)
     require(ancestorLessVertices.size == 1, "DAG must be rooted !!!")
     ancestorLessVertices.head
   }
 
-  def allVertices[V](edges: Set[(V,V)]): Set[V] = edges.flatMap(edge => Set(edge._1, edge._2))
+  def allVertices: Set[V] = edges.flatMap(edge => Set(edge._1, edge._2))
 
-  def descendantsOf[V](v: V, edges: Set[(V,V)], condition: V => Boolean = (_: V) => true): Set[V] = {
-    val (outgoingEdges, remainingEdges) = edges.partition(_._1 == v)
-    val neighborDescendants = outgoingEdges.map(_._2).filter(t => condition(t) && ancestorsOf(t, edges).forall(condition))
-    neighborDescendants ++ neighborDescendants.flatMap(descendantsOf(_, remainingEdges, condition))
+  def descendantsOf(v: V, actualEdges: Set[(V,V)] = edges)(condition: V => Boolean = (_: V) => true): Set[V] = {
+    val (outgoingEdges, remainingEdges) = actualEdges.partition(_._1 == v)
+    val neighborDescendants = outgoingEdges.map(_._2).filter(t => condition(t) && ancestorsOf(t, actualEdges).forall(condition))
+    neighborDescendants ++ neighborDescendants.flatMap(descendantsOf(_, remainingEdges)(condition))
   }
 
-  def descendantsOfRoot[V](edges: Set[(V,V)], condition: V => Boolean = (_: V) => true): Set[V] =
-    descendantsOf(root(edges), edges, condition)
+  def descendantsOfRoot(condition: V => Boolean = (_: V) => true): Set[V] =
+    descendantsOf(root)(condition)
 
-  def neighborDescendantsOf[V](v: V, edges: Set[(V,V)]): Set[V] =
+  def neighborDescendantsOf(v: V): Set[V] =
     edges.collect { case (source,target) if source == v => target }
 
-  def neighborDescendantsOfRoot[V](edges: Set[(V,V)]): Set[V] =
-    neighborDescendantsOf(root(edges), edges)
+  def neighborDescendantsOfRoot: Set[V] =
+    neighborDescendantsOf(root)
 
-  def neighborAncestorsOf[V](v: V, edges: Set[(V,V)]): Set[V] =
+  def neighborAncestorsOf(v: V): Set[V] =
     edges.collect { case (source,target) if target == v => source }
 
-  def ancestorsOf[V](v: V, edges: Set[(V,V)]): Set[V] = {
-    val (incoming, remainingEdges) = edges.partition(_._2 == v)
+  def ancestorsOf(v: V, actualEdges: Set[(V,V)] = edges): Set[V] = {
+    val (incoming, remainingEdges) = actualEdges.partition(_._2 == v)
     val neighborAncestors = incoming.map(_._1)
     neighborAncestors ++ neighborAncestors.flatMap(ancestorsOf(_, remainingEdges))
   }
 
-  def families[V](edges: Set[(V,V)]): Set[(Set[V],V)] =
+  def families: Set[(Set[V],V)] =
     edges.groupBy(_._2).mapValues(_.map(_._1)).toSet[(V,Set[V])].map { case (target, sources) => sources -> target }
 
 }
--- core/src/test/scala/gwi/saturator/DagSpec.scala
+++ core/src/test/scala/gwi/saturator/DagSpec.scala
@@ -4,58 +4,57 @@ import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, FreeSpec, Matchers}
 import org.scalatest.concurrent.ScalaFutures
 
 class DagSpec extends FreeSpec with ScalaFutures with Matchers with BeforeAndAfterEach with BeforeAndAfterAll {
-  import Dag._
 
-  private[this] val edges =
-    Set(
-      1 -> 2,
-      1 -> 3,
-      1 -> 4,
-      1 -> 5,
-      2 -> 6,
-      3 -> 6,
-      4 -> 7
+  private[this] val dag =
+    Dag[Int](
+      Set(
+        1 -> 2, 2 -> 6,
+        1 -> 3, 3 -> 6,
+        1 -> 4, 4 -> 7,
+        1 -> 5
+      )
     )
 
   "root" in {
-    assertResult(1)(root(edges))
+    assertResult(1)(dag.root)
   }
 
   "descendantsOf" in {
-    assertResult((2 to 7).toSet)(descendantsOf(1, edges))
-    assertResult(Set.empty)(descendantsOf(5, edges))
-    assertResult((2 to 7).toSet)(descendantsOf[Int](1, edges, _ => true))
-    assertResult(Set.empty)(descendantsOf[Int](5, edges, _ => true))
-    assertResult(Set.empty)(descendantsOf[Int](1, edges, x => x < 2))
-    assertResult(Set.empty)(descendantsOf[Int](1, edges, _ => false))
-    assertResult(Set(2,4,5,7))(descendantsOf[Int](1, edges, x => x != 3))
+    assertResult((2 to 7).toSet)(dag.descendantsOf(1)())
+    assertResult(Set.empty)(dag.descendantsOf(5)())
+
+    assertResult((2 to 7).toSet)(dag.descendantsOf(1)())
+    assertResult(Set.empty)(dag.descendantsOf(5)())
+    assertResult(Set.empty)(dag.descendantsOf(1)(x => x < 2))
+    assertResult(Set.empty)(dag.descendantsOf(1)(_ => false))
+    assertResult(Set(2,4,5,7))(dag.descendantsOf(1)(x => x != 3))
   }
 
   "descendantsOfRoot" in
-    assertResult((2 to 7).toSet)(descendantsOfRoot(edges))
+    assertResult((2 to 7).toSet)(dag.descendantsOfRoot())
 
   "neighborDescendantsOf" in {
-    assertResult((2 to 5).toSet)(neighborDescendantsOf(1, edges))
-    assertResult(Set.empty)(neighborDescendantsOf(5, edges))
+    assertResult((2 to 5).toSet)(dag.neighborDescendantsOf(1))
+    assertResult(Set.empty)(dag.neighborDescendantsOf(5))
   }
 
   "neighborDescendantsOfRoot" in
-    assertResult((2 to 5).toSet)(neighborDescendantsOfRoot(edges))
+    assertResult((2 to 5).toSet)(dag.neighborDescendantsOfRoot)
 
   "neighborAncestorsOf" in {
-    assertResult((2 to 3).toSet)(neighborAncestorsOf(6, edges))
-    assertResult(Set(4))(neighborAncestorsOf(7, edges))
-    assertResult(Set.empty)(neighborAncestorsOf(1, edges))
+    assertResult((2 to 3).toSet)(dag.neighborAncestorsOf(6))
+    assertResult(Set(4))(dag.neighborAncestorsOf(7))
+    assertResult(Set.empty)(dag.neighborAncestorsOf(1))
   }
 
   "ancestorsOf" in {
-    assertResult(Set(1,4))(ancestorsOf(7, edges))
-    assertResult(Set(1,2,3))(ancestorsOf(6, edges))
-    assertResult(Set.empty)(ancestorsOf(1, edges))
+    assertResult(Set(1,4))(dag.ancestorsOf(7))
+    assertResult(Set(1,2,3))(dag.ancestorsOf(6))
+    assertResult(Set.empty)(dag.ancestorsOf(1))
   }
 
   "families" in {
-    assertResult((2 to 5).map(Set(1) -> _).toSet + (Set(2,3) -> 6) + (Set(4) -> 7))(families(edges))
+    assertResult((2 to 5).map(Set(1) -> _).toSet + (Set(2,3) -> 6) + (Set(4) -> 7))(dag.families)
   }
 
 }
